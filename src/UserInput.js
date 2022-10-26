b7111079e4bac865e217e15277736a8d64b86bef
Tauri StClaire <tauristclaire@gmail.com>
2022-10-26 00:37:32 +0000

    Update App.js and UserInput.js to add tag with userinput

--- src/UserInput.js
+++ src/UserInput.js
@@ -6,15 +6,17 @@ class UserInput extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.props.addName(this.state.name);
     this.setState({ name: "" });
   };
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder="Add a new name here..."
-          value="this.state.name"
+          value={this.state.name}
           onChange={this.updateName}
         />
         <input type="submit" value="Create Name Tag" />
